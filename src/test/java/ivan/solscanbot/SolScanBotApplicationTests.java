9b15596f6ba4c6887ce0be089b4d38de71b80f94
ivanhromovyi <gromovoy1994@gmail.com>
2025-05-05 20:12:37 +0200

    added ci and checkstyle
    added ci and checkstyle
    
    added ci and checkstyle
    
    added ci and checkstyle
    
    added ci and checkstyle

--- src/test/java/ivan/solscanbot/SolScanBotApplicationTests.java
+++ src/test/java/ivan/solscanbot/SolScanBotApplicationTests.java
@@ -1,13 +1,8 @@
 package ivan.solscanbot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SolScanBotApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
